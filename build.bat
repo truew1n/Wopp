5fb1dea48fae3f20ef0265dd45500476bfc8e066
truew1n <48839784+truew1n@users.noreply.github.com>
2024-04-18 16:11:38 +0200

    Build System & Structure Adjustment

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ if not exist Build ( mkdir Build )
 if not exist Build\Debug ( mkdir Build\Debug )
 if not exist Build\Release ( mkdir Build\Release )
 
-set "IncludeDirectories=Source\Audio"
+set "IncludeDirectories=-ISource\Lib -ISource\Audio"
 
 set "AudioControllerClass=Source\Audio\AudioController.cpp"
 set "AllClasses=%AudioControllerClass%"
@@ -13,6 +13,6 @@ set "AllClasses=%AudioControllerClass%"
 set "MainFileDirectory=Source\Main.cu"
 set "OutputDirectory=Build\Debug"
 
-nvcc -o %OutputDirectory%/Main %MainFileDirectory% %AllClasses% -lcudart -lgdi32 -luser32 -lkernel32 -lwinmm -I%IncludeDirectories%
+nvcc -o %OutputDirectory%/Main %MainFileDirectory% %AllClasses% -lcudart -lgdi32 -luser32 -lkernel32 -lwinmm %IncludeDirectories%
 
 "%OutputDirectory%\Main.exe"
